2e664bc4747fcc94252a6c58dfef32300d4b08d5
trunghieu0310 <hieukoixyz123@gmail.com>
2024-10-27 18:34:18 +0700

    update

---
+++ src/main/java/com/javaweb/repository/entity/SinhVienEntity.java
@@ -0,0 +1,19 @@
+package com.javaweb.repository.entity;
+
+public class SinhVienEntity {
+	 private String maSV;
+	 private String tenSV;
+	public String getMaSV() {
+		return maSV;
+	}
+	public void setMaSV(String maSV) {
+		this.maSV = maSV;
+	}
+	public String getTenSV() {
+		return tenSV;
+	}
+	public void setTenSV(String tenSV) {
+		this.tenSV = tenSV;
+	}
+	 
+}
