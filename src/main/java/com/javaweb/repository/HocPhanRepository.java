71ca0cdf49fc601eaa78e9557d4275ebb5b29349
trunghieu0310 <hieukoixyz123@gmail.com>
2024-10-31 10:39:35 +0700

    update 2

--- src/main/java/com/javaweb/repository/HocPhanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javaweb.repository;
-
-import com.javaweb.repository.entity.HocPhanEntity;
-
-public interface HocPhanRepository {
-	HocPhanEntity findNameByMaHP(String MaHP);
-}
--- src/main/java/com/javaweb/repository/SinhVienRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javaweb.repository;
-
-import com.javaweb.repository.entity.SinhVienEntity;
-
-public interface SinhVienRepository {
-	SinhVienEntity findNameByMaSv(String MaSV);
-}
